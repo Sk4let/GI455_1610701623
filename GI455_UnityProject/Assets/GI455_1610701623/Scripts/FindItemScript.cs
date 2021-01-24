d821b6562a515a8d77c69530f5ca85b0bc4e0d9d
POKO DEV <lukacatdap@gmail.com>
2021-01-24 13:27:55 +0700

    Upload Project

---
+++ GI455_UnityProject/Assets/GI455_1610701623/Scripts/FindItemScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FindItemScript : MonoBehaviour
+{
+    public InputField _searchtxt;
+    public TextMeshProUGUI _text;
+    public GameObject _item1;
+
+    public void sendResult()
+    {
+        _text.text = _searchtxt.text;
+    }
+}
