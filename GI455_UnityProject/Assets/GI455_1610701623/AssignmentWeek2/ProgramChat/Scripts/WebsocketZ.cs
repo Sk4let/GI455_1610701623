badf5a19dfbd6b29eafce4c313f9ff61fb727e44
POKO DEV <lukacatdap@gmail.com>
2021-02-13 11:04:19 +0700

    Update Week3 change split to json

---
+++ GI455_UnityProject/Assets/GI455_1610701623/AssignmentWeek2/ProgramChat/Scripts/WebsocketZ.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WebSocketSharp;
+using TMPro;
+
+
+namespace ChatWebsocket
+{
+   public class WebsocketZ : MonoBehaviour
+   {
+       class MessageDataJson
+       {
+           public string username;
+           public string message;
+       }
+
+       public GameObject loginPanel;
+       public GameObject chatPanel;
+       public GameObject emotionPanel;
+
+       public InputField ipInput;
+       public InputField portInput;
+       public Text sendText;
+       public Text receiveText;
+       public TextMesh agentnickname;
+
+       public InputField inputText;
+       public InputField inputUsername;
+       private WebSocket webSocket;
+
+       private string tempMessageString;
+
+       bool isEmotionOpen = false;
+       public Text nickname;
+
+       public void Start()
+       {
+           var id = Random.Range(1, 999);
+           nickname.text = "Guest" + id.ToString();
+       }
+
+       public void Update() 
+       {
+           if (tempMessageString != null && tempMessageString != "")
+           {
+               MessageDataJson receiveMessageData = JsonUtility.FromJson<MessageDataJson>(tempMessageString);
+               if (receiveMessageData.username == nickname.text)
+               {
+                   sendText.text += "<color=red>" + receiveMessageData.username +  "</color> : " + receiveMessageData.message + "\n";
+                   receiveText.text += "\n";
+                   receiveText.text += "\n";
+               }
+               else 
+               {
+                   sendText.text += "\n";
+                   receiveText.text += "\n";
+                   receiveText.text += "<color=blue>" + receiveMessageData.username + "</color> : "  + receiveMessageData.message + "\n";
+               }
+
+               tempMessageString = "";
+           }
+       }
+
+       public void Connect() 
+       {
+           if (ipInput.text == "127.0.0.1" && portInput.text == "9523")
+           {
+                webSocket = new WebSocket($"ws://127.0.0.1:9523/");
+                webSocket.OnMessage += OnMessage;
+                webSocket.Connect();
+
+                loginPanel.gameObject.SetActive(false);
+                chatPanel.gameObject.SetActive(true);
+
+                if (string.IsNullOrEmpty(inputUsername.text))
+                {
+                    agentnickname.text = nickname.text;
+                }
+                else 
+                {
+                    agentnickname.text = inputUsername.text;
+                }
+
+                nickname.text = agentnickname.text;
+           }
+       }
+
+       public void SendMessage()
+       {
+           if (inputText.text == "" || webSocket.ReadyState != WebSocketState.Open)
+            return;
+
+            MessageDataJson messageData = new MessageDataJson();
+            messageData.username = nickname.text;
+            if(string.IsNullOrEmpty(inputUsername.text))
+            {
+                messageData.username = nickname.text;
+            }
+            messageData.message = inputText.text;
+
+            string toJsonStr = JsonUtility.ToJson(messageData);
+
+            webSocket.Send(toJsonStr);
+            inputText.text = "";
+       }
+
+       private void OnDestroy() 
+       {
+           if (webSocket != null)
+            {
+                webSocket.Close();
+            }
+       }
+      public void Disconnect()
+      {
+         if (webSocket != null)
+         {
+            webSocket.Close();
+         }
+
+         chatPanel.gameObject.SetActive(false);
+         loginPanel.gameObject.SetActive(true);
+
+         sendText.text = string.Empty;
+         receiveText.text = string.Empty;
+
+      }
+
+       private void OnMessage(object sender, MessageEventArgs messageEventArgs)
+       {
+           tempMessageString = messageEventArgs.Data;
+       }
+
+        public void OnOpenEmotion()
+      {
+         isEmotionOpen = !isEmotionOpen;
+         emotionPanel.gameObject.SetActive(isEmotionOpen);
+      }
+
+      public void OnExitChat() 
+      {
+          Application.Quit();
+      }
+
+   }
+}
--- NodeJS/server.js
+++ NodeJS/server.js
@@ -11,39 +11,9 @@ websocketServer.on("connection", (ws, rq) => {
    clients.push(ws); // Send Client to clients[]
 
    ws.on("message", (data) => {
-      //#checkfirst#Login as guest
-      var ext = data.split("#"); // split to ["","Checkfirst","Login as guest"]
-      var id = ext[0]; // "" Blank/
-      var type = ext[1]; // Checkfirst/
-      var msg_data = ext[2]; // Login as guest/
 
-      console.log("Client Send to Server");
-      console.log("DATA is " +ext);
-      console.log(" " +clients.length + "  in server");
-
-      // Check Type or ext[1]
-      switch (type) {
-         case "register": // Send from Client at SendAction()
-            // Assign Data between "client_id" with client in clients[]
-            ws["client_id"] = clients.length;
-            // Send to Client/
-            // $ is string type but can assign values/
-            // # is symbol for split data
-
-            // After Assign new Data send data back to client
-            ws.send(`${ws["client_id"]}#${type}#${"Login Completed"}#${clients.length}`)
-            console.log("Server send data to client");
-            // EX. 1#register#Login Completed
-            console.log(`${ws["client_id"]}#${type}#${"Login Completed"}#${"In server " +clients.length}`)
-            break;
-            
-         case "message": // Send from Client at SendAction
-            boardcast(`${id}#${type}#${msg_data}`) //Send data to clients Ex. 
-            console.log(`DATA is ${id}#${type}#${msg_data}`);
-            break;
-         default:
-            break;
-      }
+      console.log("send from client : "+data);
+      Boardcast(data);
 
    });
    // If client logout
@@ -54,11 +24,12 @@ websocketServer.on("connection", (ws, rq) => {
             break;
          }
       }
+      console.log("client disconnected.");
    });
 });
 
 // send Data to clients
-function boardcast(data) {
+function Boardcast(data) {
    for (var i = 0; i < clients.length; i++) {
       clients[i].send(data);
    }
